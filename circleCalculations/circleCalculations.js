09f436bac771c4998812c26f8d28ce953a101e8e
JayBee <jaybee@anniecanonns.com>
2019-09-11 10:00:43 -0700

    solutions to circleCalculations exercise.

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -4,7 +4,22 @@
 
 //Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
 
-function circleCalculation () {
-	//your code here
+// Problem Statement
+// Given the radius from the user, we want to find the area and the perimeter of the circle
+// Input: radius, a number
+// Create  variable to store the calculated area 
+// Create a variable to store the calculated perimeter
+// PI * radius ^ 2
+// Calculate perimeter with 2*PI*Radius
+// Return statement that is the string with the variables
+
+// Outputs: are and perimter, two numbers, bonus: output will be a concatnated string
+
+function circleCalculation (radius) {
+	var area = Math.PI * Math.pow(radius, 2);
+	var perimeter = 2 * Math.PI * radius;
+	return "The area of the circle is " + Math.round(area) + " and the perimeter of the circle is " + Math.round(perimeter) + ".";
+
+}
 
 circleCalculation(10);
