e8e607eb164f25c56f32204cc613aa8c88c87689
JayBee <jaybee@anniecanonns.com>
2019-09-16 12:04:50 -0700

    cyclicRotation exercise complete.

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -15,5 +15,21 @@
 //    For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].\
 
 function cyclicRotation(arr, rotation){
-    //Your code here...
-}
+    for (var i=0; i < rotation; i++); 
+       var arrVariable = arr.pop();
+       arr.unshift(arrVariable);
+       return arr
+    }
+
+console.log(cyclicRotation([3, 8, 9, 7, 6], 3));
+
+// Inputs: array and a number (the rotation)
+// Output: an array (with the numbers rearranged)
+// Tools: loop- the loop will run for the number of rotations that we want the array to g through
+// We ahve shuft, unshift, push and pop methods to work with arrays, we want to use them to get the last element in the array and mofe it to the beginning each time the loops run
+// Output the array once the loop has finished
+
+// Notes: // pop [i] +2
+// i < rotation
+// .pop()
+// .unshift()
