d862303b993067021fdcbf57f6c2f804d2c711eb
JayBee <jaybee@anniecanonns.com>
2019-09-19 11:33:27 -0700

    dashInsert is completed.

--- dashInsert.js
+++ dashInsert.js
@@ -25,6 +25,59 @@ console.log(result) // "1-3"
 
 **/
 
-function dashInsert(str) {
-	// Your code
+function dashInsert(num) {
+	var output = "";
+	var dashArray = num.toString().split("");
+	for (var i=0; i<dashArray.length; i++) {
+		if (dashArray[i] % 2 === 1 && dashArray[i + 1] % 2 === 1) {
+			output = output + dashArray[i] + "-";
+		} else {
+			output = output + dashArray[i];
+		}
+	}
+  return output
 }
+console.log(dashInsert(454793))
+	
+
+// Input: a number
+// Output: a modified string from the input with dashes inbetween the odd numbers of that string
+// Step 1: Create a variable to store the string 
+// Step 2: Create a variable to store the transformation of the number t an array of numbers, by first turning into first, a string, and then to an array Tools: .toString().split("")
+// 4982
+// ["4", "9", "8", "2"]
+// Step 3: Create a for loop to go through each of the numbers in the array
+	//Need a conditional statement that will test if the number is odd && if the number after it is odd
+		// If true, I want to add the number and the dash to the string
+		// If false, I want to just add the number
+
+
+
+
+
+
+
+
+// We start a for loop would loop through the items in the the array, using the modulas to identify numbers in the array that have a remainder of 1. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	if (dashArray[i] % 2 == 1)
+	arr[i = arr[i]] + " "
+}
+return dashArray.join("")
